9d5eca5bd9c79e0067ea0d5f9cefc34f4cd84f74
Nuanxinqing <nuanxinqing@gmail.com>
2024-07-12 11:23:40 +0800

    docs: 添加协议 & Dockerfile

---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+if [ ! -s /QLToolsV2/config/config.yaml ]; then
+  printf "检测到config配置目录下不存在config.yaml，从示例文件复制一份用于初始化...\n"
+  cp -fv /QLToolsV2/sample/config.yaml /QLToolsV2/config/config.yaml
+fi
+
+if [ -s /QLToolsV2/config/config.yaml ]; then
+  printf "检测到config配置目录下存在config.yaml，即将启动...\n"
+
+  ./QLToolsV2-linux-"${TARGET_ARCH}"
+
+fi
+
+exec "$@"
