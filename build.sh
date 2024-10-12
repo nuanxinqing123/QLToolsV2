ae7ba3851c29b5e9e23c655afb3edf982c08f3c4
Nuanxinqing <nuanxinqing@gmail.com>
2024-10-12 12:05:14 +0800

    style: 更新静态资源

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o QLToolsV2-linux-amd64 -ldflags '-s -w -extldflags "-static"'
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o QLToolsV2-linux-amd64 -ldflags '-s -w -extldflags "-static"'
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o QLToolsV2-linux-arm64 -ldflags '-s -w -extldflags "-static"'
 upx QLToolsV2-*
