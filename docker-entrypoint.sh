cb952b4ab8d88394f206b7ccb151b665b0c87f3a
Nuanxinqing <nuanxinqing@gmail.com>
2024-10-18 11:33:16 +0800

    feat: 更新配置文件

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ ! -s /QLToolsV2/config/config.yaml ]; then
   printf "检测到config配置目录下不存在config.yaml，从示例文件复制一份用于初始化...\n"
-  cp -fv /QLToolsV2/sample/config.yaml /QLToolsV2/config/config.yaml
+  cp -fv /QLToolsV2/config/example.config.yaml /QLToolsV2/config/config.yaml
 fi
 
 if [ -s /QLToolsV2/config/config.yaml ]; then
