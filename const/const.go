d4f72f57d0b4f8ac172680ca7c90a37031791efe
Nuanxinqing <nuanxinqing@gmail.com>
2024-10-18 11:47:42 +0800

    docs: 修改版本号

--- const/const.go
+++ const/const.go
@@ -2,14 +2,14 @@ package _const
 
 const (
 	TimeFormatAll = "2006-01-02 15:04:05" // 时间格式
-	TimeFormat    = "2006-01-02"          // 时间格式
+	// TimeFormat    = "2006-01-02"          // 时间格式
 
 	ServerBusy = "服务器正在开小差, 请稍后再试～"
 
 	CtxUserID = "UserID"
 
 	Software = "QLToolsV2"
-	Version  = "1.0.0"
+	Version  = "1.0.5"
 
 	JWTKey = "jwt_key"
 )
